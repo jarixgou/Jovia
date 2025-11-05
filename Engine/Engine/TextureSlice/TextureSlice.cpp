45a81e7c2b1276569917b6d7d114646dd94de8cd
Jarix <60546314+jarixgou@users.noreply.github.com>
2025-11-05 16:29:40 +0100

    Add TextureSlice utility for slicing textures
    Introduces Engine::TextureSliced struct and SliceTexture function to extract non-transparent cells from a texture. Updates project files to include new source, and removes obsolete build artifacts from the Release directory.

---
+++ Engine/Engine/TextureSlice/TextureSlice.cpp
@@ -0,0 +1,45 @@
+#include "TextureSlice.hpp"
+
+std::vector<Engine::TextureSliced> Engine::SliceTexture(const sf::Texture& _texture, sf::Vector2i _cellSize)
+{
+	std::vector<TextureSliced> textureSliced;
+
+	sf::Vector2u textureSize = _texture.getSize();
+
+	sf::Image image = _texture.copyToImage();
+
+	int id = 0;
+
+	for (int y = 0; y < textureSize.y / _cellSize.y; ++y)
+	{
+		for (int x = 0; x < textureSize.x / _cellSize.x; ++x)
+		{
+			TextureSliced texture;
+			texture.id = id;
+			texture.rect = { x * _cellSize.x, y * _cellSize.y,
+				_cellSize.x, _cellSize.y };
+
+			if (texture.rect.top + texture.rect.height <= textureSize.y && 
+				texture.rect.left + texture.rect.width <= textureSize.x)
+			{
+				for (int yI = texture.rect.top; yI <= texture.rect.top + texture.rect.height; ++yI)
+				{
+					for (int xI = texture.rect.left; xI <= texture.rect.left + texture.rect.width; ++xI)
+					{
+						if (image.getPixel(xI, yI).a > 0)
+						{
+							textureSliced.push_back(texture);
+
+							yI = std::numeric_limits<int>::max();
+							xI = std::numeric_limits<int>::max();
+
+							id++;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return textureSliced;
+}
--- Engine/Engine/TextureSlice/TextureSlice.hpp
+++ Engine/Engine/TextureSlice/TextureSlice.hpp
@@ -1,9 +1,21 @@
 #ifndef TEXTURE_SLICE_HPP
 #define TEXTURE_SLICE_HPP
 
-class TextureSlice
+#include <string>
+#include <vector>
+
+#include "SFML/Graphics/Rect.hpp"
+#include "SFML/Graphics/Texture.hpp"
+
+namespace Engine
 {
-	
-};
+	struct TextureSliced
+	{
+		int id;
+		sf::IntRect rect;
+	};
+
+	std::vector<TextureSliced> SliceTexture(const sf::Texture& _texture, sf::Vector2i _cellSize);
+}
 
 #endif
