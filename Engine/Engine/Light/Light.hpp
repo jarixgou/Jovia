406989867a5b7113256ff43d0aa008cde69ae605
jarixgou <60546314+jarixgou@users.noreply.github.com>
2025-11-19 00:14:39 +0100

    Expand Light class with properties and methods
    The Light class in Engine now includes radius, position, color, intensity, radial and angular falloff, and volumetric intensity properties. Getter and setter methods have been added for these attributes, improving encapsulation and usability.

--- Engine/Engine/Light/Light.hpp
+++ Engine/Engine/Light/Light.hpp
@@ -1,11 +1,36 @@
 #ifndef LIGHT__HPP
 #define LIGHT__HPP
+#include <SFML/System/Vector3.hpp>
+#include <vector>
+#include <SFML/Graphics/Color.hpp>
 
-class Light
+namespace Engine
 {
-private:
+	class Light
+	{
+	private:
+		float m_radius;
+		sf::Vector3f m_pos;
+		sf::Color m_color;
 
-public:
-};
+		float m_intensity;
+		float m_radialFalloff;
+		float m_angularFalloff;
+		float m_volumetricIntensity;
+	public:
+		Light();
+		~Light();
+
+		void SetRadius(const float& _radius);
+		void SetIntensity(const float& _intensity);
+		void SetPos(const sf::Vector3f& _pos);
+		void SetColor(const sf::Color& _color);
+
+		float GetRadius() const;
+		float GetIntensity() const;
+		const sf::Vector3f& GetPos() const;
+		const sf::Color& GetColor() const;
+	};
+}
 
 #endif
